792f5cd8d9b6ed33d1c52d92e7188b0fa9809557
dmitrynogin <dmitryn@mail.com>
2016-03-31 13:29:02 -0700

    Test EF LINQ compatibility.

--- Authorizers/Program.cs
+++ Authorizers/Program.cs
@@ -3,6 +3,7 @@ using Autofac;
 using Autofac.Features.Variance;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -25,9 +26,21 @@ namespace Authorizers
             IContainer container = builder.Build();
             var authorizer = container.Resolve<IAuthorizer>();
 
+
             Console.WriteLine(authorizer.Grants<Company, Update>(1) == true);
             Console.WriteLine(authorizer.Grants<Company, Read>(1) == true);
             Console.WriteLine(authorizer.Grants<Company, Update>(2) == false);
+
+
+            var ctx = new EContext();
+            ctx.Users.Add(new EUser());
+            ctx.SaveChanges();
+
+            var userId = new UserId(1); // compatible to EF LINQ
+            foreach (var user in from u in ctx.Users
+                              where u.Id == userId
+                              select u)
+                Console.WriteLine(user.Id);
         }
     }
 
@@ -56,5 +69,20 @@ namespace Authorizers
             Authorizer.Authorize<Company, Delete>(id);
             // ...
         }
+    }
+
+    class EContext : DbContext
+    {
+        static EContext()
+        {
+            Database.SetInitializer(new DropCreateDatabaseAlways<EContext>());
+        }
+
+        public DbSet<EUser> Users { get; set; }
+    }
+
+    class EUser
+    {
+        public int Id { get; private set; }
     }
 }
